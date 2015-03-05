d8905f8c2d565fb721181898034215056aa35d0e
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2015-03-05 15:15:59 +0000

    generate users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(user_params)
+    # password stuff here
+    if @user.save
+      flash[:success] = "Thanks for signing up"
+      # log them in here
+      redirect_to root_path
+    else
+      flash[:error] = "oops"
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Successfully updated #{@user.username}"
+      redirect_to root_path
+    else
+      flash[:error] = "oops"
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "User #{user} destroyed"
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
   root "pastries#index"
-  resources :pastries
+  resources :pastries, only: [:index, :show]
+  resources :users, except: [:show, :index] do
+    resources :pastries, except: [:index, :show]
+  end
 end
