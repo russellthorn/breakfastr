2020beca48155666443659bb1a18a5dbc0825436
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2015-03-05 14:50:55 +0000

    define RESTful actions

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -8,8 +8,45 @@ class PastriesController < ApplicationController
   end
 
   def new
+    @pastry = Pastry.new
+  end
+
+  def create
+    @pastry = Pastry.new(pastry_params)
+    @pastry.save
+    if @pastry.save
+      flash[:success] = "New pastry added"
+      redirect_to root_path
+    else
+      flash[:error] = "some will go hungry, try again"
+      render :new
+    end
   end
 
   def edit
+    @pastry = Pastry.find(params[:id])  
+  end
+
+  def update
+    @pastry = Pastry.find(params[:id])
+    if @pastry.update(pastry_params)
+      flash[:success] = "Pastry updated"
+      redirect_to pastry_path(@pastry)
+    else
+      flash[:error] = "Update fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    pastry = Pastry.find(params[:id])  
+    pastry.destroy
+    flash[:success] = "#{pastry.name} destroyed"
+    redirect_to root_path
+  end
+
+  private
+  def pastry_params
+    params.require(:pastry).permit(:name, :price_in_pence, :description)
   end
 end
