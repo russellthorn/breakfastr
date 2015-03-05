bbc0eb98010f8523668883a89abf1cbf9dd975c3
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2015-03-05 12:21:12 +0000

    loop over pastries hash

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,5 +1,16 @@
 class PastriesController < ApplicationController
   def index
+    @pastries = [
+      {
+        name: "Croissant",
+        price_in_pence: 350,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      },
+      {
+        name: "Pan au chocolat",
+        price_in_pence: 370,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        }]
   end
 
   def show
